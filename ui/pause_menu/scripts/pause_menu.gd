40d41fba699e15e8376d57e632135736ff3b98d2
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2021-01-26 00:33:47 +0000

    Fix bug causing button sfx to play in background

--- ui/pause_menu/scripts/pause_menu.gd
+++ ui/pause_menu/scripts/pause_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 onready var animation : AnimationPlayer = $AnimationPlayer
+onready var resume_button : Button = $VBoxContainer/ResumeButton
+onready var exit_button : Button = $VBoxContainer/ExitButton
 
 var is_open : bool = false
 
@@ -21,12 +23,16 @@ func open_menu():
     get_tree().paused = true
     Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
     animation.play("FadeIn")
+    resume_button.disabled = false
+    exit_button.disabled = false
     
 func close_menu():
     is_open = false
     get_tree().paused = false
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     animation.play("FadeOut")
+    resume_button.disabled = true
+    exit_button.disabled = true
 
 func _on_ResumeButton_pressed():
     if is_open:
