5b2a197b9a9a9c284d830f6e5caa9212fb2af1b4
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2021-01-29 22:14:45 +0000

    Change hole tiles to require less manual setup

--- scripts/hole_tile.gd
+++ scripts/hole_tile.gd
@@ -4,7 +4,19 @@ signal level_finished
 signal ball_entered_hole
 signal ball_exited_hole
 
-onready var finish_timer : Timer = $FinishTimer
+onready var hole_area : Area = $HoleArea
+
+var finish_timer : Timer
+
+func _ready():
+    finish_timer = Timer.new()
+    finish_timer.wait_time = 3
+    finish_timer.one_shot = true
+    finish_timer.connect("timeout", self, "_on_FinishTimer_timeout")
+    add_child(finish_timer)
+    
+    hole_area.connect("body_entered", self, "_on_HoleArea_body_entered")
+    hole_area.connect("body_exited", self, "_on_HoleArea_body_exited")
 
 func _on_HoleArea_body_entered(body):
     emit_signal("ball_entered_hole")
