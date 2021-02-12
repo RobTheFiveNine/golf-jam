0fcda4144bf8edd9a0a99287e23aaf538813e9a8
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2021-02-12 00:32:43 +0000

    Add bounce sound to ball

--- levels/base/scripts/ball.gd
+++ levels/base/scripts/ball.gd
@@ -7,12 +7,22 @@ export (NodePath) var out_of_bounds_path
 onready var raycast : RayCast = $RayCast
 onready var rigidbody : RigidBody = $RigidBody
 onready var out_of_bounds : Node = get_node(out_of_bounds_path)
+onready var bounce_sound : AudioStreamPlayer = $AudioStreamPlayer
 
 var last_position : Vector3
+var on_floor : bool
+var on_floor_last_frame : bool = true
+var current_floor : PhysicsBody
 
 func _physics_process(delta):
     var velocity_length = rigidbody.linear_velocity.length()
-    var on_floor = raycast.is_colliding()
+    on_floor = raycast.is_colliding()
+    
+    if not on_floor_last_frame and on_floor:
+        bounce_sound.play()
+        
+    on_floor_last_frame = on_floor
+    current_floor = raycast.get_collider()
 
     if on_floor and velocity_length < 0.03 and velocity_length > 0:
         rigidbody.sleeping = true
