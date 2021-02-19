2664c7b94ec778b98ebc8f2d34b63f5eb1ad6c01
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2021-02-19 20:17:40 +0000

    Change saturation in default env

--- title/scripts/title.gd
+++ title/scripts/title.gd
@@ -11,4 +11,4 @@ func _ready():
 func _on_StartButton_pressed():
     animation.play("FadeOut")
     yield(animation, "animation_finished")
-    get_tree().change_scene("res://levels/level_1/level_1.tscn")
+    get_tree().change_scene("res://levels/level_01/level_1.tscn")
